3ebf2802c0447f27d91c82714eb6effdf9b80ae9
Eugene Gordin <eugene@centerid.com>
2020-03-19 19:46:30 -0700

    Updates

---
+++ precache-manifest.d35884cedabee1fab24fcc89d2f0f5c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb56db893d5d522973eabc741ab6901",
+    "url": "/react-eugene/index.html"
+  },
+  {
+    "revision": "fc40626b3917773ae834",
+    "url": "/react-eugene/static/css/main.add22715.chunk.css"
+  },
+  {
+    "revision": "c22d4012b3d45f041201",
+    "url": "/react-eugene/static/js/2.ea1a7080.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-eugene/static/js/2.ea1a7080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc40626b3917773ae834",
+    "url": "/react-eugene/static/js/main.a9db416a.chunk.js"
+  },
+  {
+    "revision": "46abaad7d2598dcddf3b",
+    "url": "/react-eugene/static/js/runtime-main.e653d101.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-eugene/static/media/logo.5d5d9eef.svg"
+  }
+]);
